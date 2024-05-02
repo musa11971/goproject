e037e81f7dffc24908fa2506af3b8f51e17b59e6
musa11971 <mussesemou99@gmail.com>
2024-05-02 12:10:18 +0200

    wip

--- program.go
+++ program.go
@@ -2,13 +2,25 @@ package main
 
 import (
 	"fmt"
+	"time"
 
 	"github.com/fatih/color"
 	"rsc.io/quote"
 )
 
 func main() {
+	// Start the slow task
+	go slowTask()
+
+	// Do something else
 	color.HiMagenta("Hello, World, here's a quote:")
 	fmt.Println()
 	color.Magenta(quote.Go())
 }
+
+func slowTask() {
+	fmt.Print("Starting the slow task")
+	time.Sleep(30 * time.Second)
+	fmt.Print("Ending the slow task")
+}
+
